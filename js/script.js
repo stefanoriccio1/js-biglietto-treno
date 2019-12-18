92b27c60764677692b7fef5ce3a239946b3e47a7
Stefano Riccio <riccio.stefano0@gmail.com>
2019-12-18 15:56:15 +0100

    Fixata regola per fare apparire i decimali

--- js/script.js
+++ js/script.js
@@ -11,20 +11,20 @@ console.log (anni);
 
 // Il prezzo del biglietto è definito in base ai km (0.21 € al km)
 var euroKm = 0.21;
-var prezzoKm = parseInt (numeroKm * euroKm);
+var prezzoKm = parseFloat(numeroKm * euroKm).toFixed(2);
 console.log (prezzoKm);
 // ma va applicato uno sconto del 20% per i minorenni e del 40% per gli over 65.
 var over = 65
 var under = 18
 
 if (anni >= over) {
-  console.log (prezzoKm - ((prezzoKm * 40) / 100))
+  console.log (parseFloat(prezzoKm - ((prezzoKm * 40) / 100)).toFIxed(2))
 }
 else if (anni < under){
-  console.log (prezzoKm - ((prezzoKm *20) / 100));
+  console.log (parseFloat(prezzoKm - ((prezzoKm *20) / 100)).toFIxed(2));
 }
 else {
-  console.log(prezzoKm);
+  console.log(parseFloat(prezzoKm).toFixed(2));
 }
 
 // conclusione
