8b692311ff5a95c33c84c3f131f62c6df1eb2ac9
Stefano Riccio <riccio.stefano0@gmail.com>
2019-12-18 15:38:23 +0100

    inserito calcolo con sconto per over e under

--- js/script.js
+++ js/script.js
@@ -5,12 +5,24 @@ console.log (numeroKm);
  // e l’età del passeggero.
 
 var anni = parseInt(prompt('Quanti anni hai?')) ;
-console.log(anni);
+console.log (anni);
 
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
 
 // Il prezzo del biglietto è definito in base ai km (0.21 € al km)
 var euroKm = 0.21;
 var prezzoKm = parseInt (numeroKm * euroKm);
-console.log(prezzoKm);
+console.log (prezzoKm);
 // ma va applicato uno sconto del 20% per i minorenni e del 40% per gli over 65.
+var over = 65
+var under = 18
+
+if (anni >= over) {
+  console.log (prezzoKm - ((prezzoKm * 40) / 100))
+}
+else if (anni < under){
+  console.log (prezzoKm - ((prezzoKm *20) / 100));
+}
+else {
+  console.log(prezzoKm);
+}
