c963384f807e64b6a94a3b1be5423456d0f63ef0
Stefano Riccio <riccio.stefano0@gmail.com>
2019-12-18 15:30:27 +0100

    fatto calcolo prezzo al km (no sconto)

--- js/script.js
+++ js/script.js
@@ -4,9 +4,13 @@ var numeroKm = parseInt(prompt('Quanti Km devi percorrere?'));
 console.log (numeroKm);
  // e l’età del passeggero.
 
-var anni = parseInt(prompt('Quanti anni hai?'));
+var anni = parseInt(prompt('Quanti anni hai?')) ;
 console.log(anni);
 
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
 
-// Il prezzo del biglietto è definito in base ai km (0.21 € al km), ma va applicato uno sconto del 20% per i minorenni e del 40% per gli over 65.
+// Il prezzo del biglietto è definito in base ai km (0.21 € al km)
+var euroKm = 0.21;
+var prezzoKm = parseInt (numeroKm * euroKm);
+console.log(prezzoKm);
+// ma va applicato uno sconto del 20% per i minorenni e del 40% per gli over 65.
