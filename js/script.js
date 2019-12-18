af6629eef1d34609423c9b04cf66efb2cdd318c0
Stefano Riccio <riccio.stefano0@gmail.com>
2019-12-18 15:17:00 +0100

    insertia richiesta km

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Calcolo prezzo del treno:
+// Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere
+var numeroKm = parseInt(prompt('Quanti Km devi percorrere?'));
+console.log (numeroKm);
+ // e l’età del passeggero.
+
+
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
+
+// Il prezzo del biglietto è definito in base ai km (0.21 € al km), ma va applicato uno sconto del 20% per i minorenni e del 40% per gli over 65.
